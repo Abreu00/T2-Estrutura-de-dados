3d9429759d3063c53024a79af160d318bd29481a
Fabio de Abreu <fabiowabreu@gmail.com>
2019-06-18 21:09:41 -0300

    Trie insert aparentemente funcionando -- testes simples

--- trie.hpp
+++ trie.hpp
@@ -4,51 +4,57 @@
 #include <string>
 #include <stdexcept>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 namespace structures {
 
 class Trie {
-	public:
-	void insert(string key) {
-        root->insert(root, key, 0);
-        //cout << "executando insert() com key = " << key << "\n";
+ public:
+	Trie() {
+    root = new Node('\0');
+  }
+  
+  void insert(string key) {
+    root->insert(root, key, 0);
+    //cout << "executando insert() com key = " << key << "\n";
+  }
+
+ private:
+  struct Node {
+    char letter;
+    vector<Node *> sons;
+    std::size_t pos;
+    std::size_t length;
+    
+    explicit Node(char letter){
+      this->letter = letter;
+      this->sons = vector<Node*>();
     }
-    private:
-    	struct Node {
-        char letter;
-        vector<Node *> sons;
-        std::size_t pos;
-        std::size_t length;
-        
-        explicit Node(char letter){
-            this->letter = letter;
+
+    Node* insert(Node* node, string key, int d) {
+      bool hasChild = false;
+      Node* next_node;
+      for (int i = 0; i < sons.size(); ++i) {
+        if (sons[i]->letter == key[d]) {
+          next_node = sons[i];
+          hasChild = true;
+          break;
         }
+      }
 
-        Node* insert(Node* node, string key, int d) {
-          
-            bool hasChild = false;
+      if (!hasChild) {
+        next_node = new Node(key[d]);
+        sons.push_back(next_node);
+      }
 
-            for (int i = 0; i < sons.size(); ++i) {
-              if (sons[i]->letter == key[d]) {
-                node = sons[i];
-                hasChild = true;
-                break;
-              }
-            }
-
-            if (!hasChild) {
-              Node* node = new Node(key[d]);
-              sons.push_back(node);
-            }
-
-            bool last = key.size() == d + 1;
-            if (!last) {
-              node->insert(node, key, d + 1);
-            }
-        }
-    };
+      bool last = key.size() == d + 1;
+      if (!last) {
+        next_node->insert(node, key, d + 1);
+      }
+    }
+};
     Node* root;        
   };
 } // Namespace structures
